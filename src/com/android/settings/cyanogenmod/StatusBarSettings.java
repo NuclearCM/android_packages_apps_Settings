747b8985430fc02c33cf62533a0db7007ea19133
Moludos <moledo.alex@gmail.com>
2016-01-21 03:40:50 +0100

    Fix left qs quickpulldown

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -23,6 +23,7 @@ import android.content.Context;
 /*import android.content.DialogInterface;*/
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
