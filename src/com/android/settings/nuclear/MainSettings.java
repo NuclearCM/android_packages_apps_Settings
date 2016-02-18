b28c25264e8821d8dd965c2428c556e7ebab921b
Moludos <moledo.alex@gmail.com>
2016-02-18 15:19:43 +0100

    Reconfigure Nuclear Menu, and Tint icons

--- src/com/android/settings/nuclear/MainSettings.java
+++ src/com/android/settings/nuclear/MainSettings.java
@@ -16,18 +16,36 @@
 
 package com.android.settings.nuclear;
 
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.os.SystemProperties;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.preference.SeekBarPreference;
+import android.provider.MediaStore;
 import android.provider.Settings;
-import android.util.SparseArray;
-import android.provider.SearchIndexableResource;
-import android.content.Context;
-import android.util.SettingConfirmationHelper;
+import android.view.Display;
+import android.view.Window;
+import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -35,11 +53,23 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.search.SearchIndexableRaw;
 import com.android.settings.Utils;
+import android.util.SparseArray;
+import android.content.res.Resources;
+
+
+import android.provider.SearchIndexableResource;
+import android.content.Context;
+import android.util.SettingConfirmationHelper;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.DataOutputStream;
 
 public class MainSettings extends SettingsPreferenceFragment implements Indexable {
     private static final String KEY_PERSO = "perso";
@@ -48,7 +78,10 @@ public class MainSettings extends SettingsPreferenceFragment implements Indexabl
     private static final String KEY_BLOQ = "bloq";
     private static final String KEY_MISC = "misc";
 
+	private static final String CATEGORY_PERFORMANCE = "performance_category";
 
+	private static final String KEY_SYNAPSE = "key_synapse";
+	private static final String KEY_KERNEL_ADIUTOR = "key_kernel_adiutor";
 
     /*    private static SparseArray<String> allKeyTitles(Context context) {
         final SparseArray<String> rt = new SparseArray<String>();
@@ -90,10 +123,52 @@ public class MainSettings extends SettingsPreferenceFragment implements Indexabl
             null,
             null
         );*/
+	Boolean checkPerformance = Settings.System.getInt(getContentResolver(), Settings.System.PERFORMANCE_APP, 0) == 1;
+
+        if(checkPerformance) {
+           PreferenceScreen synapse = (PreferenceScreen) findPreference(KEY_SYNAPSE);
+           getPreferenceScreen().removePreference(synapse);
+        } else {
+           PreferenceScreen kernelAdiutor = (PreferenceScreen) findPreference(KEY_KERNEL_ADIUTOR);
+           getPreferenceScreen().removePreference(kernelAdiutor);
+        }
     }
+
     @Override
     protected int getMetricsCategory() {
         return 1;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference.getKey().equals(KEY_SYNAPSE)) {
+            final String appPackageName = "com.af.synapse";
+            try {
+                getActivity().getPackageManager().getPackageInfo(appPackageName, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+                try {
+                    getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (ActivityNotFoundException ex) {
+                    getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+                return true;
+            }
+            return false;
+        } else if (preference.getKey().equals(KEY_KERNEL_ADIUTOR)) {
+            final String appPackageName = "com.grarak.kerneladiutor";
+            try {
+                getActivity().getPackageManager().getPackageInfo(appPackageName, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+                try {
+                    getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (ActivityNotFoundException ex) {
+                    getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+                return true;
+            }
+            return false;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
        /* // === Indexing ===
 
